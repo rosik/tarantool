b3131c7edf78b243e188c755ad5c1d3f5b2747ab
Aleksandr Lyapunov <alyapunov@tarantool.org>
2021-11-22 17:31:16 +0300

    box: add pin/unping infostructure for func cache
    There are cases when we need to be sure the a function is not
    deleted and/or removed from func cache. For example constraints:
    they must hold a pointer to struct func while it's very hard to
    determine whether there'a constraint that points to given func.
    
    Implement func pin/unpin for this purpose. You can pin a func to
    declare that the func must not be deleted. To have to unpin it
    when the func is not needed anymore.

--- src/box/func_cache.c
+++ src/box/func_cache.c
@@ -9,12 +9,25 @@
 #include "assoc.h"
 
 static struct mh_i32ptr_t *funcs;
+/**
+ * Hash tuple for func name to a ring list of subscriptions.
+ * If at least one subscription exist for a name the (an only then) there's
+ * a node in this table with `val` member pointing to one of subscriptions
+ * (that is struct func_cache_subscription). Several subscriptions with the
+ * same name are linked via `link` member.
+ */
+static struct mh_i32ptr_t *pinned_funcs;
 static struct mh_strnptr_t *funcs_by_name;
+
+const char *func_cache_holder_type_strs[HOLDER_TYPE_MAX] = {
+	"constraint",
+};
 
 void
 func_cache_init()
 {
 	funcs = mh_i32ptr_new();
+	pinned_funcs = mh_i32ptr_new();
 	funcs_by_name = mh_strnptr_new();
 }
 
@@ -30,6 +43,7 @@ func_cache_destroy()
 		func_delete(func);
 	}
 	mh_i32ptr_delete(funcs);
+	mh_i32ptr_delete(pinned_funcs);
 	mh_strnptr_delete(funcs_by_name);
 }
 
@@ -50,11 +64,11 @@ func_cache_insert(struct func *func)
 void
 func_cache_delete(uint32_t fid)
 {
+	assert(mh_i32ptr_find(pinned_funcs, fid, NULL) == mh_end(pinned_funcs));
 	mh_int_t k = mh_i32ptr_find(funcs, fid, NULL);
 	if (k == mh_end(funcs))
 		return;
-	struct func *func = (struct func *)
-		mh_i32ptr_node(funcs, k)->val;
+	struct func *func = (struct func *) mh_i32ptr_node(funcs, k)->val;
 	mh_i32ptr_del(funcs, k, NULL);
 	k = mh_strnptr_find_str(funcs_by_name, func->def->name,
 				strlen(func->def->name));
@@ -80,3 +94,64 @@ func_by_name(const char *name, uint32_t name_len)
 	return (struct func *) mh_strnptr_node(funcs_by_name, func)->val;
 }
 
+void
+func_cache_pin(struct func *func, struct func_cache_holder *holder,
+	       enum func_cache_holder_type type)
+{
+	holder->type = type;
+	uint32_t fid = func->def->fid;
+	assert(mh_i32ptr_find(funcs, fid, NULL) != mh_end(funcs));
+
+	mh_int_t pos = mh_i32ptr_find(pinned_funcs, fid, NULL);
+	if (pos == mh_end(pinned_funcs)) {
+		/* No holders yet. The new ring will consist of one holder. */
+		rlist_create(&holder->link);
+		const struct mh_i32ptr_node_t new_node = {fid, holder};
+		mh_i32ptr_put(pinned_funcs, &new_node, NULL, NULL);
+	} else {
+		/* There are some holders. Add to their ring. */
+		struct func_cache_holder *another = mh_i32ptr_node(pinned_funcs,
+								   pos)->val;
+		assert(another != NULL);
+		rlist_add_tail(&another->link, &holder->link);
+	}
+}
+
+void
+func_cache_unpin(struct func *func, struct func_cache_holder *holder)
+{
+	uint32_t fid = func->def->fid;
+	assert(mh_i32ptr_find(funcs, fid, NULL) != mh_end(funcs));
+
+	mh_int_t pos = mh_i32ptr_find(pinned_funcs, fid, NULL);
+	assert(pos != mh_end(pinned_funcs));
+	struct mh_i32ptr_node_t *node = mh_i32ptr_node(pinned_funcs, pos);
+
+	if (rlist_empty(&holder->link)) {
+		/* This is the last element of the ring. Delete entry. */
+		mh_i32ptr_del(pinned_funcs, pos, NULL);
+	} else {
+		/* There are more holders, need to remove holder from ring. */
+		if (node->val == holder) {
+			/* Also need need to repoint to some other holder. */
+			struct func_cache_holder *next =
+				rlist_next_entry(holder, link);
+			const struct mh_i32ptr_node_t new_node = { fid, next };
+			mh_i32ptr_put(pinned_funcs, &new_node, NULL, NULL);
+		}
+		rlist_del(&holder->link);
+	}
+}
+
+bool
+func_cache_is_pinned(struct func *func, enum func_cache_holder_type *type)
+{
+	assert(mh_i32ptr_find(funcs, func->def->fid, NULL) != mh_end(funcs));
+	mh_int_t pos = mh_i32ptr_find(pinned_funcs, func->def->fid, NULL);
+	if (pos == mh_end(pinned_funcs))
+		return false;
+	struct mh_i32ptr_node_t *node = mh_i32ptr_node(pinned_funcs, pos);
+	struct func_cache_holder *holder = node->val;
+	*type = holder->type;
+	return true;
+}
